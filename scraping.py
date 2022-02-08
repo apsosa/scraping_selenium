1c3ca204eb8817cc3de59af8172ed26b4d317a4c
Patricio Sosa <patdeveloper.data@gmail.com>
2022-02-08 19:54:03 -0300

    woeking with ccs class

--- scraping.py
+++ scraping.py
@@ -3,30 +3,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-website= "https://www.starz.com/ar/es/movies"
 s = Service('./chromedriver')
 driver = webdriver.Chrome(service=s)
 
-from selenium import webdriver
-
-website= "https://www.starz.com/ar/es/movies"
-path= "./chromedriver"
-
 
 #import ipdb;ipdb.set_trace()
-driver.get('http://www.google.com/')
+driver.get('https://www.starz.com/ar/es/movies')
 
 time.sleep(5) # Let the user actually see something!
 
 #search_box = driver.find_element_by_name('q')
+import ipdb;ipdb.set_trace()
 
-search_box = driver.find_element(By.NAME, 'q')
+search_box = driver.find_element(By.CSS_SELECTOR, 'div.text-uppercase')
 
-search_box.send_keys('ChromeDriver')
 
-search_box.submit()
-
-time.sleep(5) # Let the user actually see something!
-
-driver.quit()
 #import ipdb;ipdb.set_trace()
